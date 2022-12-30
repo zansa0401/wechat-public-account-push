4345b4d805fea00b10196949319afeb6a5f79499
zansa0401 <91767597+zansa0401@users.noreply.github.com>
2022-12-30 17:46:09 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -8,41 +8,41 @@
 const USER_CONFIG = {
 
   // 使用微信测试号：公众号APP_ID
-  APP_ID: '',
+  APP_ID: 'wx14aff8758753dc52',
 
   // 使用微信测试号：公众号APP_SECRET
-  APP_SECRET: '',
+  APP_SECRET: '1e70163e9b3860f7823533af20a529a3',
 
-  PROVINCE: '广东',
-  CITY: '惠州',
+  PROVINCE: '河南',
+  CITY: '驻马店',
 
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '柯柯',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o3EKt5y9wY5NJ5PwRgR4f1kWiVJQ',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'g7IP0SHVo_30YRcLSaECJ_OfC-7zAHkTx3h4NlleqPk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '08-08',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '柯柯', year: '2004', date: '06-23',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '柯柯', year: '2004', date: '08-08',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '相识纪念日', year: '2022', date: '11-13',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        { keyword: 'love_day', date: '2022-11-16' },
         // 结婚纪念日
         { keyword: 'marry_day', date: '2022-09-09' },
       ],
@@ -51,13 +51,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: '_U_GQuvkAi4MUTF4OBViYNCFHJ-SRcp7XiPmHkn6BqU',
 
   CALLBACK_USERS: [
     {
-      name: '自己',
+      name: 'zansa',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o3EKt57vzIbuIvoRfh4Pm7pTJgQo',
     }
   ],
 
